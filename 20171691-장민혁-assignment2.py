f080ae92d3174a0179a8683525293712fe8539ab
Min-Hyuk98 <wkdalsgur85@kookmin.ac.kr>
2017-09-07 09:45:13 +0900

    multiply

--- 20171691-장민혁-assignment2.py
+++ 20171691-장민혁-assignment2.py
@@ -1,14 +1,15 @@
 num = 0 
-sum = 0
+factorial = 1
 
  
 while 1:
     num = int(input("Enter a number "))
     if num == 0:
-        sum = 1
+        print("1! = 1")
     elif num == -1:
         break
-    for i in range(1,num+1):
-        sum = sum + i
-    print(str(num)+ "!" , "=" ,sum)
-    sum = 0
+    else:
+        for i in range(1,num+1):
+            factorial = i*factorial
+        print(str(num)+ "!" , "=" ,factorial)
+        factorial = 1
