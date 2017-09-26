154a955f0f17301efe96c55ce3dcf53c34441795
tmp2147483647 <reverse@kookmin.ac.kr>
2017-09-26 09:22:30 +0900

    Modified code

--- assignment4_20171692장우석.py
+++ assignment4_20171692장우석.py
@@ -1,5 +1,16 @@
-def combination(n,m):
+def change(n,m):
+    while n < 0 or m < 0 or n<m: #음수이거나 n<m이면 입력을 다시 받기
+        print("Wrong number")
+        n = int(input("Enter n: "))
+        m = int(input("Enter m: "))
+    
+    tmp=0
+    tmp=n-m
+    if tmp != 0 and m > tmp: # r을 작은 수로 바꾸기
+        m=tmp
+    return n,m
 
+def combination(n,m):
     if m == 0 or n == m:
         return 1
 
@@ -11,5 +22,6 @@ def combination(n,m):
 
 n = int(input("Enter n: "))
 m = int(input("Enter m: "))
+n,m = change(n,m)
 solution = combination(n,m)
 print(n,"C",m,"=",solution)
