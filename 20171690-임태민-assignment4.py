adf72213d4b769d6eb87b0f90e0e2d91fa97a17c
ltm-lab <taemin328@naver.com>
2017-09-26 09:29:32 +0900

    Add files via upload

---
+++ 20171690-임태민-assignment4.py
@@ -0,0 +1,14 @@
+import operator as op
+
+def comb(n,r):
+    if r == 0:
+        return 1
+    elif r == n:
+        return 1
+    else:
+        return comb(n-1,r-1) + comb(n-1,r)
+
+n = int(input('N: '))
+r = int(input('R: '))
+
+print(str(n)+'C'+str(r)+' = '+str(comb(n,r)))
