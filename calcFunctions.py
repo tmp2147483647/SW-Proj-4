f47108fa35e0defc3cb70d4f930cdf1ab437ee52
Min-Hyuk98 <wkdalsgur85@kookmin.ac.kr>
2017-11-14 07:16:53 +0900

    장민혁 계산기 first commit

---
+++ calcFunctions.py
@@ -0,0 +1,49 @@
+from math import factorial as fact
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+
+
+def decToRoman(numStr):
+    try:
+        numInt = int(numStr)
+        roman = ''
+
+        while int(numInt) > 0:
+            for i, r in num_map:
+                while numInt >= i:
+                    roman += r
+                    numInt -= i
+    except:
+        return 'Error!'
+    return roman
+
+
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+
+
+
