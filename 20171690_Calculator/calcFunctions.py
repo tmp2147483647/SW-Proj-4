56f6d198b44b7bdabec301aff8caa5fb8f6f9e68
ltm-lab <taemin328@naver.com>
2017-11-14 02:03:24 +0900

    Calc upgrade ver.

---
+++ 20171690_Calculator/calcFunctions.py
@@ -0,0 +1,32 @@
+def fact(x):
+    if x == 1 or x == 0:
+        return 1
+ 
+    return x * fact(x - 1)
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
