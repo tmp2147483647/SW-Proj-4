150bf1f9eaba414d2da7db5c8c7367d20b53d213
tmp2147483647 <reverse@kookmin.ac.kr>
2017-11-14 09:35:02 +0900

    Add files via upload

--- calcFunctions.py
+++ calcFunctions.py
@@ -1,5 +1,6 @@
 from math import factorial as fact
 
+
 def factorial(numStr):
     try:
         n = int(numStr)
@@ -24,26 +25,16 @@ def binToDec(numStr):
         r = 'Error!'
     return r
 
-
-
 def decToRoman(numStr):
-    try:
-        numInt = int(numStr)
-        roman = ''
-
-        while int(numInt) > 0:
-            for i, r in num_map:
-                while numInt >= i:
-                    roman += r
-                    numInt -= i
-    except:
-        return 'Error!'
-    return roman
-
-
-num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+    return 'dec -> Roman'
 
 
 
 
+function_Dic = {
+    'factorial (!)': factorial,
+    '-> binary': decToBin,
+    'binary -> dec': binToDec,
+    '-> roman': decToRoman,
+    'roman -> dec': decToRoman,
+}
