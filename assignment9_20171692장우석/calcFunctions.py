fc7cf0e69df42c29cde472187b091ff14afdfa13
tmp2147483647 <reverse@kookmin.ac.kr>
2017-11-22 12:41:02 +0900

    Add files via upload

---
+++ assignment9_20171692장우석/calcFunctions.py
@@ -0,0 +1,82 @@
+from math import factorial as fact
+
+
+romans = {
+    1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+    100:'C', 90:'XC', 50:'L', 40:'XL',
+    10: 'X', 9: 'IX', 5: 'V', 4: 'IV',
+    1: 'I'
+}
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr)
+        r = "0b" + str(n)
+    except:
+        r = "Error!"
+
+    return int(r, 2)
+
+def decToRoman(numStr):
+    try:
+        n = int(numStr)
+        result = ""
+        for value in sorted(romans.keys(), reverse=True):
+            while n >= value:
+                result += romans[value]
+                n -= value
+    except:
+        result = "Error!"
+    return result
+
+def romanToDec(Str):
+    try:
+        tmp = 0
+        for value in sorted(romans.keys(), reverse=True):
+            check = len(romans[value])
+
+            while(check == 1 and Str[0:1] == romans[value]):
+                Str = Str.replace(romans[value], "", 1)
+                tmp += value
+
+            while (check == 2 and Str[0:2] == romans[value]):
+                Str = Str.replace(romans[value], "", 1)
+                tmp += value
+
+
+    except:
+        tmp = "Error!"
+    return tmp
+function_Dic = {
+    'factorial (!)': factorial,
+    '-> binary': decToBin,
+    'binary -> dec': binToDec,
+    '-> roman': decToRoman,
+    'roman -> dec': romanToDec,
+}
+
+'''
+functionMap = [
+    ('factorial (!)', factorial),
+    ('-> binary', decToBin),
+    ('binary -> dec', binToDec),
+    ('-> roman', decToRoman),
+    ('roman -> dec', decToRoman),
+]
+'''
